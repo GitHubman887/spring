ba3b322a6d1a4370f394275e29bbb38e3b6454a0
GitHubman887 <352914885@qq.com>
2023-03-17 15:06:37 +0800

    init

---
+++ spring-debug/src/main/java/com/mashibing/test/T.java
@@ -0,0 +1,22 @@
+package com.mashibing.test;
+
+public class T {
+    private int t;
+
+
+
+    public int getT() {
+        return t;
+    }
+
+    public void setT(int t) {
+        this.t = t;
+    }
+
+    @Override
+    public String toString() {
+        return "T{" +
+                "t=" + t +
+                '}';
+    }
+}
---
+++ spring-debug/src/main/java/com/mashibing/test/TestCycle.java
@@ -0,0 +1,19 @@
+package com.mashibing.test;
+
+import com.mashibing.MyClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.StandardEnvironment;
+
+public class TestCycle {
+
+    public static void main(String[] args) {
+//        new StandardEnvironment();
+        ApplicationContext ac = new ClassPathXmlApplicationContext("test.xml");
+//        T bean = ac.getBean(T.class);
+//        System.out.println(bean);
+//        B bean1 = ac.getBean(B.class);
+//        System.out.println(bean1);
+
+    }
+}
